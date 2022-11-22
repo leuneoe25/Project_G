c1c9553fa2dac332354fbe07ba45f334162e3114
jinryang <84126292+jinryang@users.noreply.github.com>
2022-11-22 20:56:32 +0900

    Presenter
    make Mesods

---
+++ Project/Assets/02.Script/MVP/Presenter.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Presenter : MonoBehaviour
+{
+    #region Singleton
+    private static Presenter instance = null;
+    void Awake()
+    {
+        if (null == instance)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    public static Presenter Instance
+    {
+        get
+        {
+            if (null == instance)
+            {
+                return null;
+            }
+            return instance;
+        }
+    }
+    #endregion
+    public GameObject store;
+
+    void Start()
+    {
+        store = GameObject.Find("Canvases").transform.FindChild("Store").gameObject;
+    }
+
+    public void OpenMenu()
+    {
+
+    }
+    public void OpenUpgrade()
+    {
+
+    }
+    public void OpenFriendUpgrade()
+    {
+
+    }
+    public void OpenGetFriend()
+    {
+
+    }
+
+    public void OpenStore()
+    {
+        
+       // Go to 'Store Model'
+
+        //store.SetActive(true);
+        
+    }
+
+    public void OpenProtect()
+    {
+        /*
+        SceneController -> InGame Scene
+        
+        */
+    }
+}
