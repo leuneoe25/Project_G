51bbb298cf1d7b020676e65e3be9f75d246e050d
ksm1028 <84126337+ksm1028@users.noreply.github.com>
2022-11-28 20:29:39 +0900

    Change IngameScene and change JsonCode
    InGameScene
    -make some button
    -make folder
    JsonScript
    -make Enum

---
+++ Project/Assets/02.Script/Json/SaveALoadManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Newtonsoft.Json;
+
+public enum characterClass
+{
+    Archery,
+    Kendo,
+    Clean,
+    MarineSports,
+    Science,
+    Boxing,
+    Band,
+    Cooking,
+    Library
+}
+
+class Data
+{
+    int money;
+    int NumberOfLockedCharacter = 0;
+
+    string[,] character = new string[4, 20];
+    
+    
+
+    //가지고 있는 캐릭터 bool 1 2 3 4
+}
+
+public class SaveALoadManager : MonoBehaviour
+{
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
