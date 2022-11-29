a0155f94720743a3d0d6626bd00c55165b629650
ksm1028 <84126337+ksm1028@users.noreply.github.com>
2022-11-30 00:22:17 +0900

    Json

--- Project/Assets/02.Script/Json/SaveALoadManager.cs
+++ Project/Assets/02.Script/Json/SaveALoadManager.cs
@@ -18,50 +18,60 @@ public enum characterClass
 
 class Data
 {
-    int stage;
+    public int stage;
 
     /// <summary>
     /// Save method: character name + character class (Enum) & N line = N Stars (ex : 4line "kokoko, Science" = star : 4 roll : science name : kokoko)
     /// </summary>
-    string[,] character = new string[4, 20];
+    public string[,] character = new string[4, 20];
 
     /// <summary>
     /// 0 : wood
     /// 1 : iron
     /// 2 : glass
     /// </summary>
-    int[] bulidEle = new int[3];
+    public int[] bulidEle = new int[3];
 
     /// <summary>
     /// 0 : coin
     /// 1 : SchoolPoint(SP)
     /// 2 : Budget
     /// </summary>
-    int[] CommonGoods = new int[3];
+    public int[] CommonGoods = new int[3];
 
     /// <summary>
     /// 0 : Attack
     /// 1 : Support
     /// 2 : Debuff
     /// </summary>
-    int[] GemOftTranscendence = new int[3];
+    public int[] GemOftTranscendence = new int[3];
 
-    float[] CharacterStat = new float[20];
+    public float[] CharacterStat = new float[20];
 
-    int LevelOFCastle;
+    public int LevelOFCastle;
     
     /// <summary>
     /// int : Serial number which tutorial have
     /// bool : tutorial was finded by player
     /// </summary>
-    Queue<KeyValuePair<int, bool>> tuto = new Queue<KeyValuePair<int, bool>>();
+    public Queue<KeyValuePair<int, bool>> tuto = new Queue<KeyValuePair<int, bool>>();
 }
 
 public class SaveALoadManager : MonoBehaviour
 {
+    Data player = new Data() { stage = 1, character = { }, bulidEle = { }, CommonGoods = { }, CharacterStat = { }, LevelOFCastle = 1  };
+
     void Start()
     {
-        
+        string JsonData = JsonUtility.ToJson(player);
+
+        print(JsonData);
+
+    }
+
+    void LoadJson(string data)
+    {
+        player = JsonUtility.FromJson<Data>(data);
     }
 
     void Update()
