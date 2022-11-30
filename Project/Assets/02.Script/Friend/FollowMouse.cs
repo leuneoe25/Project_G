fcbf4ecb65e03db17f280dd1807b15588f3b3fb2
jinryang <84126292+jinryang@users.noreply.github.com>
2022-11-30 19:01:47 +0900

    FollowMouse

---
+++ Project/Assets/02.Script/Friend/FollowMouse.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMouse : MonoBehaviour
+{
+    private void Start()
+    {
+
+    }
+    void Update()
+    {
+        Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        if (pos.y < -1.8f)
+            pos.y = -3.5f;
+        else
+            pos.y = 1.7f;
+        transform.position = new Vector3(Mathf.Clamp(pos.x, -5f, 5f), pos.y, 0f);
+    }
+}
+    
