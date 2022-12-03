324eada24694986c66aaedd7f6323e0f1932605d
spfkeh <83802641+spfkeh@users.noreply.github.com>
2022-12-03 15:54:21 +0900

    MapInfo

--- Project/Assets/02.Script/UI/SetGameMenuUI.cs
+++ Project/Assets/02.Script/UI/SetGameMenuUI.cs
@@ -5,24 +5,20 @@ using UnityEngine.UI;
 
 public class SetGameMenuUI : MonoBehaviour
 {
+    [SerializeField] private SceneManagement scene;
     [Header("Goods Text")]
     [SerializeField] private Text SP_text;
     [SerializeField] private Text[] Buildingtext;
+
+    [SerializeField] private Button StoreButton;
     void Start()
     {
-        //GoodsSystem.Instance.SetBuildingGoods(101, 101, 101);
-        //GoodsSystem.Instance.SetSP(100);
+        StoreButton.onClick.AddListener(scene.LoadStore);
 
         SP_text.text = GoodsSystem.Instance.GetSP().ToString();
         for(int i = 0; i < Buildingtext.Length;i++)
         {
             Buildingtext[i].text = GoodsSystem.Instance.GetBuildingGoods(i).ToString();
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
