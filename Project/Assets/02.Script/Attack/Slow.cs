bddef2ec0937f6339ef336b0b2f367f9a861cfa3
ksm1028 <84126337+ksm1028@users.noreply.github.com>
2022-12-05 00:36:29 +0900

    science

--- Project/Assets/02.Script/Attack/Slow.cs
+++ Project/Assets/02.Script/Attack/Slow.cs
@@ -6,16 +6,48 @@ public class Slow : MonoBehaviour
 {
     float time = 3;
     float secondTime = 1f;
+    bool isGround;
     void Start()
     {
         
     }
+    float originalslow;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Monster"))
+        {
+            Monster monster = collision.GetComponent<Monster>();
+            originalslow = monster.Speed / 3;
+            monster.Speed -= originalslow;
+        }
+        if(collision.CompareTag("Ground"))
+        {
+            isGround = true;
+        }
+    }
 
+    private void OnTriggerExit2D(Collider2D collision)
+    {
 
+        if (collision.CompareTag("Monster"))
+        {
+            Monster monster = collision.GetComponent<Monster>();
+            monster.Speed += originalslow;
+        }
+    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        time -= Time.deltaTime;
+        if(time <= 0)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + 1000f, transform.position.z);
+            secondTime -= Time.deltaTime;
+            if(secondTime <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
     }
 }
