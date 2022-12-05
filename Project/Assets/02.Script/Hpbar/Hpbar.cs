1951aafa8a083538371d426acaa4aaa925bacc75
jinryang <84126292+jinryang@users.noreply.github.com>
2022-12-06 00:40:44 +0900

    HpBar

---
+++ Project/Assets/02.Script/Hpbar/Hpbar.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Hpbar : MonoBehaviour
+{
+    [SerializeField] Image image;
+    [SerializeField] Friend friend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
