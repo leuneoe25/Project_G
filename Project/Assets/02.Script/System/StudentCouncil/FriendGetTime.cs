04b119f6bda4429f24e5aa7d51792962dc5e8e57
ksm1028 <84126337+ksm1028@users.noreply.github.com>
2022-12-05 23:01:08 +0900

    council

--- Project/Assets/02.Script/System/StudentCouncil/FriendGetTime.cs
+++ Project/Assets/02.Script/System/StudentCouncil/FriendGetTime.cs
@@ -6,11 +6,35 @@ using UnityEngine.UI;
 public class FriendGetTime : MonoBehaviour
 {
     [SerializeField] Button button;
+    [SerializeField] Button X;
     [SerializeField] GameObject Object;
     [SerializeField] Text[] text;
     [SerializeField] string[] TextText;
     public int cost;
-    
-    
 
+    private void Start()
+    {
+        button.onClick.AddListener(() => PushButton());
+    }
+
+    private void TextSet()
+    {
+        text[0].text = TextText[0];
+        text[1].text = TextText[1] + ColleagueSystem.Instance.Getcolleagues().Count + " / 18";
+        text[2].text = TextText[2] + cost.ToString();
+    }
+
+    public void PushButton()
+    {
+        if(!Object.activeSelf)
+        {
+            Object.SetActive(true);
+            TextSet();
+            X.onClick.AddListener(() => PushButton());
+        }
+        else
+        {
+            Object.SetActive(false);
+        }
+    }
 }
